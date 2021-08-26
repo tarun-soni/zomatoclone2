d63c83f0b8a88219b72a257cedc2820bc2900b68
tarun-soni <soni.tarun26@gmail.com>
2021-08-26 17:49:16 +0530

    RN boiler plate

---
+++ App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>test</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
