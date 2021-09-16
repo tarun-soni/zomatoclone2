5da57289044e23b2312e841713b43ce0ef3455ed
tarun-soni <soni.tarun26@gmail.com>
2021-09-16 19:44:39 +0530

    added recipe info screen

--- App/screens/PrivateScreens/RecipeTab/RecipeInfoScreen.js
+++ App/screens/PrivateScreens/RecipeTab/RecipeInfoScreen.js
@@ -1,11 +1,85 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { Animated, Image, Text, View, SafeAreaView } from 'react-native'
+import { COLORS, FONTS, SIZES } from '../../../constants/theme'
 
-const RecipeInfoScreen = () => {
+const HEADER_HEIGHT = 350
+
+const RecipeInfoScreen = ({ navigation, route }) => {
+  const [selectedRecipe, setSelectedRecipe] = useState(null)
+
+  const scrollY = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    const { recipe } = route.params
+    setSelectedRecipe(recipe)
+  }, [route])
+
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+      <Animated.FlatList
+        data={selectedRecipe?.ingredients}
+        keyExtractor={item => `${item.id}`}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={() => (
+          <View>
+            <Text>header</Text>
+          </View>
+        )}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: { contentOffset: { y: scrollY } },
+            },
+          ],
+          { useNativeDriver: true },
+        )}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingHorizontal: 30,
+              marginVertical: 5,
+            }}
+          >
+            {/* icon */}
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 50,
+                width: 50,
+                borderRadius: 5,
+                backgroundColor: COLORS.lightGreen,
+              }}
+            >
+              <Image source={item.icon} style={{ width: 40, height: 40 }} />
+            </View>
+            {/* desc */}
+            <View
+              style={{
+                paddingHorizontal: 20,
+                flex: 1,
+                justifyContent: 'center',
+              }}
+            >
+              <Text style={{ ...FONTS.body3 }}>{item.description}</Text>
+            </View>
+            {/* quanitity */}
+
+            <View
+              style={{
+                alignItems: 'flex-end',
+                justifyContent: 'center',
+              }}
+            >
+              <Text style={{ ...FONTS.body3 }}>{item.quantity}</Text>
+            </View>
+          </View>
+        )}
+      />
       <Text>RECIPE INFO</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
